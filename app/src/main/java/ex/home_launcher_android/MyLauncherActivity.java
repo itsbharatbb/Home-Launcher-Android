c939ba9f6a4b61cdf575e7f90e492141e6532abf
B <itsbharatbb@gmail.com>
2015-08-23 11:02:17 +0530

    Populated Activities with barebone gesture code

--- app/src/main/java/ex/home_launcher_android/MyLauncherActivity.java
+++ app/src/main/java/ex/home_launcher_android/MyLauncherActivity.java
@@ -2,36 +2,65 @@ package ex.home_launcher_android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MyLauncherActivity extends Activity {
-
+    RelativeLayout launcherLayout;
+    GestureDetector detector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_launcher);
+        launcherLayout = (RelativeLayout) findViewById(R.id.launcherLayout);
+        detector = new GestureDetector(this,ogl);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my_launcher, menu);
-        return true;
+    public boolean onTouchEvent(MotionEvent event) {
+        this.detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    GestureDetector.OnGestureListener ogl = new GestureDetector.OnGestureListener() {
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public void onShowPress(MotionEvent e) {
+            Toast.makeText(getBaseContext(),"onShowPress",Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Toast.makeText(getBaseContext(),"OnSingleTap",Toast.LENGTH_SHORT).show();
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            Toast.makeText(getBaseContext(), "OnScroll", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Toast.makeText(getBaseContext(),"onLongPress",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Toast.makeText(getBaseContext(),"OnFling",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+    };
+
+
+
 }
