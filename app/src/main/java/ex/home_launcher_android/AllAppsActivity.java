8e0fcba65755720240ffc9564cb12a27fd4bc1ac
B <itsbharatbb@gmail.com>
2015-08-23 10:21:37 +0530

    Added Two Activities

---
+++ app/src/main/java/ex/home_launcher_android/AllAppsActivity.java
@@ -0,0 +1,15 @@
+package ex.home_launcher_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AllAppsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_apps);
+    }
+
+
+}
